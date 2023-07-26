3208e97b758b2ec40bb75ae42ee15def6f42519c
Luciano Montanares <lmontanareso@gmail.com>
2023-07-25 22:25:35 -0400

    change settings

--- scraper/biobio/settings.py
+++ scraper/biobio/settings.py
@@ -20,7 +20,7 @@ NEWSPIDER_MODULE = "biobio.spiders"
 # USER_AGENT = "biobio (+http://www.yourdomain.com)"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = False
+ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 # CONCURRENT_REQUESTS = 32
@@ -30,7 +30,7 @@ ROBOTSTXT_OBEY = False
 # See also autothrottle settings and docs
 # DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
-# CONCURRENT_REQUESTS_PER_DOMAIN = 16
+CONCURRENT_REQUESTS_PER_DOMAIN = 10
 # CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
